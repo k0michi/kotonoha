2e6f53cc743b192408b1c578fc4577ae377839db
Komichi <k0michi@koyomi.co>
2022-10-07 11:43:43 +0900

    Use HashRouter

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -3,16 +3,16 @@ import * as ReactDOM from 'react-dom/client';
 import Root from './root';
 import Model from './model';
 import { ModelProvider } from 'kyoka';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import './style.css';
 
 const model = new Model();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
+  <HashRouter>
     <ModelProvider model={model}>
       <Root />
     </ModelProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
